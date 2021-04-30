a0a7b56db45d72c95694dd68893d7af1202fc48a
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-04-30 15:08:15 +0200

    Drop support for old microservices

--- build-and-push-images.sh
+++ build-and-push-images.sh
@@ -6,7 +6,4 @@ docker image tag gateway fridgy/gateway
 docker push fridgy/gateway
 docker image tag products fridgy/products
 docker push fridgy/products
-docker image tag groceries fridgy/groceries
-docker push fridgy/groceries
-docker image tag recipes fridgy/recipes
-docker push fridgy/recipes
+
--- build-images-local.sh
+++ build-images-local.sh
@@ -7,11 +7,3 @@ cd ..
 cd products
 ./gradlew bootJar -Pprod jibDockerBuild
 cd ..
-
-cd groceries
-./gradlew bootJar -Pprod jibDockerBuild
-cd ..
-
-cd recipes
-./gradlew bootJar -Pprod jibDockerBuild
-cd ..
